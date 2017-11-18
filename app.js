94b96ba6a6410db7c9fbbc266e04c4ae3f5d2af6
Paul Huang <paul_hzq@hotmail.com>
2017-11-18 16:30:00 -0500

    add contact page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var nodemailer = require('nodemailer');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var about = require('./routes/about');
+var contact = require('./routes/contact');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/about', about);
+app.use('/contact', contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
